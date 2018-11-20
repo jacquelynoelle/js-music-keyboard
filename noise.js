40952af4a98fa24a71f56de79ec2896793a2e1b4
jackie <jacquelyn.cheng@me.com>
2018-11-19 16:57:08 -0800

    Added audio for clicks and keybindings

--- noise.js
+++ noise.js
@@ -1,3 +1,16 @@
 $(document).ready( function() {
-  // your code here
+  const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+  for (let note of notes) {
+    $(`.${note}`).on("click", function() {
+      $(`#${note}Audio`).load();
+      $(`#${note}Audio`).get(0).play();
+    });
+  }
+
+  $('body').on("keypress", function(event) {
+    const note = event.key;
+    $(`#${note}Audio`).load();
+    $(`#${note}Audio`).get(0).play();
+  });
 });
